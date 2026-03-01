211403a6954ffac0b0635dcd859babbe11190609
Bikramveer Gill <bikramveergill@gmail.com>
2026-02-28 19:41:51 -0800

    updated API URL to work with vercel and E2 IP

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/applications';
+
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/applications';
 
 const api = {
     getAllApplications: () => axios.get(API_BASE_URL),
